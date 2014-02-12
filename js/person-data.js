8059de3a9c1518d4e5882078311bedf7677925ab
Brad Schvaneveldt <bschvanz@hotmail.com>
2014-02-12 14:30:14 -0700

    Adding project files

---
+++ js/person-data.js
@@ -0,0 +1,12 @@
+var people = [
+	{ name: "Frank L. Smith", familyName: "Smith", birthDayNumber: 2452163.5, birthCountry: "United States" },
+	{ name: "John P. Smith", familyName: "Smith", birthDayNumber: 2431205.5, birthCountry: "Australia" },
+	{ name: "John P. Smith", familyName: "Smith", birthDayNumber: 2418938.000000, birthCountry: "United States" },
+	{ name: "Allan H. Johnson", familyName: "Johnson", birthDayNumber: 2443986.5, birthCountry: "Canada" },
+	{ name: "Alfonzo B. Dominiti", familyName: "Jackson", birthDayNumber: 2445873.5, birthCountry: "Italy" },
+	{ name: "Frank L. Allred", familyName: "Allred", birthDayNumber: 2423413.5, birthCountry: "Australia" },
+	{ name: "Robert S. Williams", familyName: "Williams", birthDayNumber: 2426934.5, birthCountry: "United States" },
+	{ name: "John P. Smith", familyName: "Smith", birthDayNumber: 2448961.5, birthCountry: "United States" },
+	{ name: "John P. Smith", familyName: "Smith", birthDayNumber: 2415446.5, birthCountry: "Canada" },
+	{ name: "John P. Smith", familyName: "Smith", birthDayNumber: 2450831.5, birthCountry: "United Kingdom" }
+];
